9f5e4ee1aef51036eaa4f3754c7857d270d17958
ricardodev9 <164495350+ricardodev9@users.noreply.github.com>
2024-11-13 05:14:58 +0100

    Indicaciones para las rutas

--- index.php
+++ index.php
@@ -2,14 +2,18 @@
 require_once 'autoload.php';  
 require_once 'config.php';    
 
-if(isset($_GET['view']) && $_GET['view'] == 'players'){
+/**
+ * Indicaciones para las rutas
+ */
 
+if(isset($_GET['team']) && $_GET['team']){
+    $teamController->getTeamById($_GET['team'], $teamController->getCaptains($_GET['team']));
+    $playerController->getPlayersByTeam($_GET['team']);
+}else if(isset($_GET['player']) && $_GET['player'] !=''){
+    $playerController->getPlayerById($_GET['player'], $teamController->getTeamsObject());
 }else{
-    if(isset($_GET['team']) && $_GET['team']){
-        $teamController->getTeamById($_GET['team']);
-    }else{
-        $teamController->getTeams();
-    }
+    $teamController->getTeams();
 }
+
 
 ?>
