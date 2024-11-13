db4034578a3dc679e29e527ccb3d680a0d046e5b
ricardodev9 <164495350+ricardodev9@users.noreply.github.com>
2024-11-13 05:14:25 +0100

    Aviso de fallo en la conexión e inicialización de instancia para el Player

--- config.php
+++ config.php
@@ -1,20 +1,27 @@
 <?php
-
-// Define la constante BASE_PATH para la ruta base de la aplicación
-define('BASE_PATH', dirname(__FILE__)); // Asegura que la ruta base se establezca correctamente.
+define('BASE_PATH', dirname(__FILE__));
 
 // Cargar el autoloader
-require_once BASE_PATH . '/autoload.php';  // Cargar el autoloader
+require_once BASE_PATH . '/autoload.php'; 
 
 // Crear una instancia de la clase Database para manejar la conexión a la base de datos
 $database = new Database();
 $db = $database->connect();
 
-// Crear una instancia del modelo y la vista
-$teamModel = new TeamModel($db);  // Instancia del modelo
-$teamView = new TeamView();  // Instancia de la vista
+//si hubo algún error, mostrará este mensaje
+if(!$db){
+    die("<p>Importa el archivo .sql de la raíz del proyecto a la BBDD</p>");
+}
 
-// Crear una instancia del controlador y pasarle el modelo y la vista
-$teamController = new TeamController($teamModel, $teamView);  // Ahora se pasan ambos parámetros
+// Crear una instancia de los modelos y las vistas
+$teamModel = new TeamModel($db);  
+$teamView = new TeamView(); 
+$playerModel = new PlayerModel($db); 
+$playerView = new PlayerView();
+
+// Crear instancias de los controladores y pasarles sus respectivos modelos y vistas
+$teamController = new TeamController($teamModel, $teamView);  
+$playerController = new PlayerController($playerModel, $playerView);  
+
 
 ?>
