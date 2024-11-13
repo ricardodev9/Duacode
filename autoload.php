09a9e4525baf15ce8e9364d9776a526db35044fc
ricardodev9 <164495350+ricardodev9@users.noreply.github.com>
2024-11-13 05:14:46 +0100

    Incluir MVC de Player

--- autoload.php
+++ autoload.php
@@ -1,15 +1,20 @@
 <?php
-
+/**
+ * Importar las clases necesarias
+ */
 spl_autoload_register(function ($class_name) {
-    // Determinar la ruta del archivo de la clase, asumiendo que las clases están en /class/team o /class
-    $class_name = str_replace('\\', '/', $class_name);  // Si utilizas namespaces
+    $class_name = str_replace('\\', '/', $class_name);  
     $file = BASE_PATH . '/class/team/' . $class_name . '.php';
+    $file2 = BASE_PATH . '/class/player/' . $class_name . '.php';
     $fileDB = BASE_PATH . '/class/DB.php';
     if (file_exists($file)) {
         require_once $file;
     }
     if(file_exists($fileDB)){
         require_once BASE_PATH . '/class/DB.php';
+    }
+    if(file_exists($file2)){
+        require_once $file2;
     }
 });
 
